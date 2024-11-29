a20dab359a5d603dedb04a66a0f1b266401df929
Alex Seisler <aseisler7045@stevenscollege.edu>
2024-11-29 07:18:36 -0500

    linking main temp

---
+++ Main-Template/script.js
@@ -0,0 +1,5 @@
+// JavaScript to trigger the modal on page load
+window.onload = function () {
+    var myModal = new bootstrap.Modal(document.getElementById('introModal'));
+    myModal.show();
+  };

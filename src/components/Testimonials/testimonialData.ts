d59cbbfecb13a89a1145177ffc97bb513986635c
Alex Seisler <aseisler7045@stevenscollege.edu>
2025-09-13 17:44:49 -0400

    Full repo transfer

---
+++ src/components/Testimonials/testimonialData.ts
@@ -0,0 +1,23 @@
+export const testimonials = [
+  {
+    text: "Alex Seisler is a standout in website development, combining creativity and attention to detail to deliver polished, functional results. His professionalism and commitment to quality make him a reliable choice for exceptional web solutions.",
+    image: '/content/images/Ashaf.png',
+    name: "Ashraful Mohammad",
+    role: "Senior Developer",
+    company: "Tech Innovations"
+  },
+  {
+    text: "Alex's dedication to his craft and his ability to approach each project with strategic focus are truly remarkable. I have no doubt that ACS Results is built on a foundation of excellence, and anybody partnering with them will see meaningful results and success.",
+    image: '/content/images/Browne.png',
+    name: "Dr. Laughner",
+    role: "Professor",
+    company: "Education Institute"
+  },
+  {
+    text: "ACS Results provided our students with a professional way to showcase their skills and career readiness. It’s a valuable bridge between classroom learning and real-world opportunities.",
+    image: "/content/images/Spencer.png",
+    name: "Spencer Harper",
+    role: "Software Coach",
+    company: "Digital Solutions Inc."
+  }
+];
